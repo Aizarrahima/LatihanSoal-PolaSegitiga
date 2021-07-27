54b07ec5bedc0b18005e5c6e8b58f3a8a4bc1f36
Aizar Rahima Suprayitno <70837905+Aizarrahima@users.noreply.github.com>
2021-07-27 15:32:50 +0700

    Update B.java

--- src/PolaSegitiga/B.java
+++ src/PolaSegitiga/B.java
@@ -7,7 +7,7 @@ package PolaSegitiga;
 public class B {
 
     public static void main(String[] main) {
-        int a, b; // inisialisasi variabel a dan b
+        int a, b; // inisialisasi variabel a dan b dengan tipe data integer
         for (a = 1; a <= 5; a++) { // sebagai perulangan luar
             System.out.println(""); // untuk membentuk baris sesuai pola perulangan
             for (b = a; b <= 5; b++) { // sebagai perulangan dalam mulai dari 5 hingga 1
